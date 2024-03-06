35b0964ad75782b56504784c1c01aa8ae770bf93
SAIGUSA Tomotada <ts@nectarition.jp>
2024-03-06 13:23:38 +0900

    event-schedule.ics追加

---
+++ src/pages/event-schedule.ics.ts
@@ -0,0 +1,51 @@
+import ics, { type EventAttributes } from 'ics'
+import eventLib from '../libs/event'
+
+export const GET = async () => {
+
+  const fetchedEvents = await eventLib.getEventsAsync();
+  const events = fetchedEvents
+    .map<EventAttributes>(e => {
+      const date = e.date
+      const description = [
+        `ジャンル: ${eventLib.convertGenre(e.genreType)}`,
+        `イベント種類: ${eventLib.convertEventType(e.eventType).name}`,
+        ...(e.remarks && [
+          '---',
+          e.remarks
+        ])
+      ].join('\n')
+
+      return {
+        title: `${e.place.prefecture}: ${e.name}`,
+        description,
+        location: e.place.name,
+        start: [
+          date.getFullYear(),
+          date.getMonth() + 1,
+          date.getDate()
+        ],
+        end: [
+          date.getFullYear(),
+          date.getMonth() + 1,
+          date.getDate()
+        ],
+        startInputType: 'utc',
+        startOutputType: 'local',
+        endInputType: 'utc',
+        endOutputType: 'local',
+        organizer: {
+          name: e.organizer.name
+        }
+      }
+    })
+
+  const { error: _, value } = ics.createEvents(events)
+
+  return new Response(
+    value, {
+    headers: {
+      "Content-Type": "text/plain; charset=UTF-8"
+    }
+  })
+}
